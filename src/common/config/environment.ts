a969e41cb48ead03b10ffe7f11df55acd26d64be
yooseungwan <swan1032@naver.com>
2024-08-30 17:10:24 +0900

    [FEAT] Add Configuration setting

---
+++ src/common/config/environment.ts
@@ -0,0 +1,9 @@
+import { IsIn, IsString } from 'class-validator';
+
+export class Environment {
+  @IsIn(['production', 'test', 'development'])
+  NODE_ENV = process.env.NODE_ENV;
+
+  @IsString()
+  SERVICE_NAME = process.env.SERVICE_NAME;
+}
