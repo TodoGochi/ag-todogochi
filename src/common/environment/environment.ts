f86ec749135fa63a009e3ad98784a822e680c826
유승완 <swan1032@naver.com>
2024-09-05 21:59:57 +0900

    [FIX] Remove Kakao environment

--- src/common/environment/environment.ts
+++ src/common/environment/environment.ts
@@ -1,11 +1,4 @@
-import { plainToClass, Type } from 'class-transformer';
-import {
-  IsIn,
-  IsNumberString,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
-import { KakaoAuthConfig } from './object-config/kakao-auth.config';
+import { IsIn, IsNumberString, IsString } from 'class-validator';
 
 export class Environment {
   @IsIn(['production', 'test', 'development'])
@@ -19,13 +12,4 @@ export class Environment {
 
   @IsString()
   USER_SERVER_ADDR = process.env.USER_SERVER_ADDR;
-
-  // Kakao Auth
-  @ValidateNested()
-  @Type(() => KakaoAuthConfig)
-  KAKAO_AUTH: KakaoAuthConfig = plainToClass(KakaoAuthConfig, {
-    clientId: process.env.KAKAO_CLIENT_ID,
-    clientSecret: process.env.KAKAO_CLIENT_SECRET,
-    redirectUri: process.env.KAKAO_REDIRECT_URI,
-  });
 }
