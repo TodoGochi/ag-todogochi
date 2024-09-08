eaf6c646d28c9fab33cddefa10fd150cd33891fe
유승완 <swan1032@naver.com>
2024-09-09 03:27:59 +0900

    [FIX] Password, Nickname validation

--- src/auth/dto/auth-req.dto.ts
+++ src/auth/dto/auth-req.dto.ts
@@ -1,5 +1,6 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Length, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class SignUpReqBodyDto {
   @ApiProperty({ example: 'example@example.com', description: 'email' })
@@ -7,6 +8,12 @@ export class SignUpReqBodyDto {
   email: string;
 
   @ApiProperty({ example: 'nickname', description: 'nickname' })
+  @IsString()
+  @Length(2, 15, { message: 'Nickname must be between 2 and 15 characters.' })
+  @Matches(/^[a-zA-Z0-9가-힣]*$/, {
+    message:
+      'Nickname can only contain letters, numbers, or Korean characters.',
+  })
   nickName: string;
 
   @ApiProperty({
@@ -14,6 +21,13 @@ export class SignUpReqBodyDto {
     description: 'password',
   })
   @IsString()
+  @Length(8, 20, {
+    message: 'Password must be between 8 and 20 characters long.',
+  })
+  @Transform(({ value }) => value.toLowerCase())
+  @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, {
+    message: 'Password must include letters, numbers, and special characters.',
+  })
   password: string;
 }
 
@@ -30,5 +44,12 @@ export class SignInReqBodyDto {
 
   @ApiProperty({ example: 'password', description: 'password' })
   @IsString()
+  @Length(8, 20, {
+    message: 'Password must be between 8 and 20 characters long.',
+  })
+  @Transform(({ value }) => value.toLowerCase())
+  @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, {
+    message: 'Password must include letters, numbers, and special characters.',
+  })
   password: string;
 }
