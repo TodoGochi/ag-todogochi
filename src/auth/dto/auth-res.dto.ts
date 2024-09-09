cf701a0e5235ea66684b666a53b06b208c52328a
유승완 <swan1032@naver.com>
2024-09-10 02:50:34 +0900

    [FIX] SignUpResDto

--- src/auth/dto/auth-res.dto.ts
+++ src/auth/dto/auth-res.dto.ts
@@ -1,28 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserDto } from 'src/user/dto/user-res.dto';
 
-export class TokensDto {
+export class SignUpResDto {
+  @ApiProperty({ description: 'User information', type: UserDto })
+  user: UserDto;
+
   @ApiProperty({
     description: 'JWT Access Token',
     example:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImVtYWlsIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsImlhdCI6MTcyNTQxMzI2NywiZXhwIjoxNzI1NDk5NjY3fQ.W9-X66v8tZx0HD_opF86envhbhqwijNuC6T948loAXQ',
   })
   accessToken: string;
-
-  @ApiProperty({
-    description: 'JWT Refresh Token',
-    example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImVtYWlsIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsImlhdCI6MTcyNTQxMzI2NywiZXhwIjoxNzI2MDE4MDY3fQ.FnN-s73eAbXJJtkg4xRd7NcGAXf7zQMMDSBmQhZ5qVc',
-  })
-  refreshToken: string;
-}
-
-export class SignUpResDto {
-  @ApiProperty({ description: 'User information', type: UserDto })
-  user: UserDto;
-
-  @ApiProperty({ description: 'JWT tokens', type: TokensDto })
-  tokens: TokensDto;
 }
 
 export class SignInResDto extends SignUpResDto {}
