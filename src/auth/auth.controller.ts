ef870ae74f6211fff8eef4e94d87091d72333245
yooseungwan <swan1032@naver.com>
2024-09-09 14:51:00 +0900

    [FIX] cookie options

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -48,7 +48,8 @@ export class AuthController {
     const { user, tokens } = response.data;
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'none',
+      secure: true,
       maxAge: REFRESH_TOKEN_MAX_AGE,
     });
 
@@ -73,7 +74,8 @@ export class AuthController {
     const { user, tokens } = response.data;
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'none',
+      secure: true,
       maxAge: REFRESH_TOKEN_MAX_AGE,
     });
 
