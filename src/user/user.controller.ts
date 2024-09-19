36eca257db97bdc98dddc000490a4c6d45601a44
yooseungwan <swan1032@naver.com>
2024-09-19 18:36:07 +0900

    [FIX] getcointransactions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { Swagger } from 'src/common/decorators/swagger.decorator';
 import { UserIdReqParamDto } from './dto/user-req.dto';
 import { Response, Request } from 'express';
 import { USER_DOCS } from './constant/user.swagger';
+import { Role } from 'src/common/decorators/roles.decorator';
+import { ROLE } from 'src/common/constants/role.constant';
+import { AccessTokenGuard } from 'src/common/core/guards/access-token.guard';
+import { RolesGuard } from 'src/common/core/guards/role.guard';
 
 @Swagger(USER_DOCS.USER_CONTROLLER)
 @Controller('user')
@@ -11,14 +24,18 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Swagger(USER_DOCS.GET_COIN_TRANSACTIONS_BY_USER_ID)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Role(ROLE.MEMBER)
   @Get(':userId/coin-transactions')
   async getCoinTransactionByUserId(
     @Param() params: UserIdReqParamDto,
     @Res() res: Response,
+    @Req() req: Request,
   ) {
-    const response = await this.userService.getCoinTransactionsByUserId(
-      params.userId,
-    );
+    const response = await this.userService.getCoinTransactionsByUserId({
+      userId: params.userId,
+      req,
+    });
 
     return res.status(response.status).json(response.data);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ROLE } from 'src/common/constants/role.constant';
+import { ApiError } from 'src/common/error/api.error';
 import { UserService as UserServer } from 'src/provider/server/services/user.service';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userService: UserServer) {}
 
-  async getCoinTransactionsByUserId(userId: number) {
+  async getCoinTransactionsByUserId(input: { userId: number; req: any }) {
+    if (
+      input.req.user.role < ROLE.ADMIN &&
+      input.req.user.userId !== input.userId
+    ) {
+      throw new ApiError('AG-0001');
+    }
     const response = await this.userService.get({
-      path: `/user/${userId}/coin-transactions`,
+      path: `/user/${input.userId}/coin-transactions`,
     });
 
     return { data: response.data, status: response.status };
