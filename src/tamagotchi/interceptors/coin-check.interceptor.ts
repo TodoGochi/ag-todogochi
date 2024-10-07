956075128f2dfb36b1dbf6826b5d335273623c8e
Minwoo <alsdn8055@naver.com>
2024-10-07 13:38:45 +0900

    [FIX] change userId to tamagotchiId

--- src/tamagotchi/interceptors/coin-check.interceptor.ts
+++ src/tamagotchi/interceptors/coin-check.interceptor.ts
@@ -8,17 +8,28 @@ import { Observable, throwError } from 'rxjs';
 import { Request } from 'express';
 import { ApiError } from 'src/common/error/api.error';
 import { UserService as UserServer } from 'src/provider/server/services/user.service';
+import { TamagotchiService as TamagotchiServer } from 'src/provider/server/services/tamagotchi.service';
 
 @Injectable()
 export class CoinCheckInterceptor implements NestInterceptor {
-  constructor(private readonly userService: UserServer) {}
+  constructor(
+    private readonly userService: UserServer,
+    private readonly tamagotchiService: TamagotchiServer,
+  ) {}
 
   async intercept(
     context: ExecutionContext,
     next: CallHandler,
   ): Promise<Observable<any>> {
     const request: Request = context.switchToHttp().getRequest();
-    const userId = request.body.userId;
+    const tamagotchiId = request.params.id;
+    const result = await this.tamagotchiService.get({
+      path: `/tamagotchi/${tamagotchiId}/statusbytamagotchi`,
+    });
+
+    const tamagotchi = result.data;
+
+    console.log(tamagotchi);
 
     // 요청 경로에서 어떤 서비스인지 확인
     const path = request.route.path;
@@ -41,7 +52,7 @@ export class CoinCheckInterceptor implements NestInterceptor {
     try {
       // 외부 서버로 코인 정보 요청
       const response = await this.userService.get({
-        path: `/user/${userId}`,
+        path: `/user/${tamagotchi.user_id}`,
       });
 
       const coinBalance = response.data.coin;
@@ -60,7 +71,7 @@ export class CoinCheckInterceptor implements NestInterceptor {
 
       // 코인 차감 요청
       await this.userService.post({
-        path: `/user/${userId}/coin-transactions`,
+        path: `/user/${tamagotchi.user_id}/coin-transactions`,
         data: {
           changeAmount: -requiredCoins, // 필요한 코인 개수만큼 차감
           description: `Coin deduction for ${serviceName} service: ${requiredCoins} coins`,
